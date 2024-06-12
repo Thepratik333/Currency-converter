b7af35630311f47e5961cc174c61e7cdde5262b1
Thepratik333 <pratikpanchal858@gmail.com>
2024-06-13 00:38:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Converter from './currencyConverter/Converter'
+
+import './App.css'
+
+function App() {
+  return (
+      <div>
+      <Converter/>
+      </div>
+  )
+}
+
+export default App
